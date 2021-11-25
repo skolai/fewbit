92a4c08a7bb496804515758925ee035795388d63
Daniel Bershatsky <daniel.bershatsky@gmail.com>
2021-11-25 03:33:17 +0300

    Clean root project directory

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
-from os.path import dirname
 from setuptools import setup
-from torch.utils.cpp_extension import BuildExtension, CppExtension
 
-setup(name='fewbit',
-      ext_modules=[
-          CppExtension('fewbit',
-                       ['fewbit/fewbit.cc', 'fewbit/python_module.cc'],
-                       include_dirs=[dirname(__file__)])
-      ],
-      cmdclass={'build_ext': BuildExtension})
+setup(name='fewbit')
