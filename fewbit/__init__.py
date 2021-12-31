030e102a37e86b36d3af7c8f38e35b3077b8b14a
Daniel Bershatsky <daniel.bershatsky@gmail.com>
2021-12-31 17:49:37 +0300

    Comply PyTorch modules naming convention

--- fewbit/__init__.py
+++ fewbit/__init__.py
@@ -18,11 +18,11 @@ from . import functional  # noqa: F401
 from .approx import StepWiseFunction, approximate  # noqa: F401
 
 # Import piecewise activation functions.
-from .module import (  # noqa: F401
+from .modules import (  # noqa: F401
     Hardshrink, Hardsigmoid, Hardtanh, LeakyReLU, ReLU, ReLU6, Softshrink,
     Threshold)
 
 # Import continous activation functions.
-from .module import (  # noqa: F401
+from .modules import (  # noqa: F401
     CELU, ELU, GELU, Hardswish, LogSigmoid, Mish, SELU, Sigmoid, SiLU,
     Softplus, Softsign, Tanh, Tanhshrink)
