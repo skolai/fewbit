fd3b69f7729f184d6dae8cd8ac3651964fda7450
Daniel Bershatsky <daniel.bershatsky@gmail.com>
2022-01-01 12:50:58 +0300

    Implement linear layer with column-row sampling

---
+++ fewbit/modules/linear.py
@@ -0,0 +1,75 @@
+#   encoding: utf-8
+#   filename: linear.py
+
+import torch as T
+import torch.autograd
+import torch.nn.functional as F
+
+from typing import Optional
+
+__all__ = ('LinearCRS', )
+
+
+class LinearCRSFunc(T.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, input: T.Tensor, weight: T.Tensor,
+                bias: Optional[T.Tensor], nopairs: int) -> T.Tensor:
+        in_features = weight.shape[1]
+        proba = 1 / in_features  # Uniform col-row distribution.
+        factor = proba * nopairs
+
+        randomness = T.randint(0, in_features, (nopairs, )).to(weight.device)
+        bins = T.bincount(randomness, minlength=in_features)
+        pairs, = T.nonzero(bins, as_tuple=True)
+        counts = bins[pairs]
+        scale = counts.type(T.float32) / factor
+        input_proj = input[..., pairs] @ T.diag(scale)
+        ctx.save_for_backward(input_proj, weight, bias, pairs)
+        return F.linear(input, weight, bias)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input_proj, weight, bias, pairs = ctx.saved_tensors
+
+        input_grad = None
+        if ctx.needs_input_grad[0]:
+            input_grad = grad_output @ weight
+
+        weight_grad = None
+        if ctx.needs_input_grad[1]:
+            weight_grad = T.zeros_like(weight)
+            weight_grad[..., pairs] = T.einsum('...i,...j->ij', grad_output,
+                                               input_proj)
+
+        bias_grad = None
+        if ctx.needs_input_grad[2]:
+            bias_grad = T.einsum('...i->i', grad_output)
+
+        return (input_grad, weight_grad, bias_grad, None)
+
+
+linear_crs = LinearCRSFunc.apply
+
+
+class LinearCRS(T.nn.Linear):
+    """Class LinearCRS defines PyTorch layer based torch.nn.Linear module. It
+    approximate gradients (of weights) with column-row sampling (CRS) and
+    stored subset of input columns for backward phase.
+    """
+
+    def __init__(self,
+                 in_features: int,
+                 out_features: int,
+                 bias: bool = True,
+                 device=None,
+                 dtype=None,
+                 nopairs: Optional[int] = None) -> None:
+        super().__init__(in_features, out_features, bias, device, dtype)
+        self.nopairs: int = nopairs or out_features // 2
+
+    def forward(self, input: T.Tensor) -> T.Tensor:
+        return linear_crs(input, self.weight, self.bias, self.nopairs)
+
+    def extra_repr(self) -> str:
+        return f'{super().extra_repr()} + nopairs={self.nopairs}'
---
+++ fewbit/modules/linear_test.py
@@ -0,0 +1,79 @@
+#   encoding: utf-8
+#   filename: linear_test.py
+
+from unittest import TestCase
+
+import torch as T
+
+from fewbit.modules.linear import LinearCRS
+
+
+class TestLinearCRS(TestCase):
+
+    @staticmethod
+    def clone_module(module: T.nn.Module) -> T.nn.Module:
+        cloned = T.nn.Linear(module.in_features, module.out_features,
+                             module.bias is not None)
+        for name, param in cloned.named_parameters():
+            param.data = getattr(module, name).clone()
+        return cloned
+
+    @staticmethod
+    def estimate_grad(module: T.nn.Module, xs: T.Tensor, repeat: int = 1):
+        weight_grad = T.zeros_like(module.weight)
+        if (with_bias := module.bias is not None):
+            bias_grad = T.zeros_like(module.bias)
+
+        for _ in range(repeat):
+            module.zero_grad()
+            xs.grad = None
+            ys = module(xs)
+            ys.backward(T.ones_like(ys))
+            weight_grad += module.weight.grad
+            if with_bias:
+                bias_grad += module.bias.grad
+
+        weight_grad /= repeat
+        if with_bias:
+            bias_grad /= repeat
+
+        if with_bias:
+            return xs.grad, weight_grad, bias_grad
+        else:
+            return xs.grad, weight_grad, None
+
+    def setUp(self):
+        self.rng = T.random.manual_seed(42)
+
+    def test_forward(self):
+        for bias in (False, True):
+            with self.subTest(bias=bias):
+                module = LinearCRS(8, 4, bias)
+                module_cloned = self.clone_module(module)
+
+                with T.no_grad():
+                    xs = T.randn((128, 8))
+                    ys = module(xs)
+                    zs = module_cloned(xs)
+
+                rel = T.linalg.norm(zs - ys) / T.linalg.norm(zs)
+                self.assertAlmostEqual(0, rel.item(), delta=1e-6)
+
+    def test_backward(self):
+        for bias in (False, True):
+            with self.subTest(bias=bias):
+                module = LinearCRS(8, 4, bias)
+                module_cloned = self.clone_module(module)
+
+                xs = T.randn((128, 8), requires_grad=True)
+                ys, ps, ss = self.estimate_grad(module, xs, 4096)
+                zs, qs, ts = self.estimate_grad(module_cloned, xs)
+
+                err_input = T.linalg.norm(ys - zs) / T.linalg.norm(zs)
+                err_weight = T.linalg.norm(ps - qs) / T.linalg.norm(qs)
+                self.assertAlmostEqual(0, err_input.item(), delta=1e-6)
+                self.assertAlmostEqual(0, err_weight.item(), delta=5e-2)
+
+                if bias:
+                    err_bias = T.linalg.norm(ss - ts) / T.linalg.norm(ts)
+                    self.assertAlmostEqual(0, err_bias.item(), delta=1e-6)
