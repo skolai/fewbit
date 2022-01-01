cf07bf567b2ff975678ad16a7e03abb8fc4366b8
Daniel Bershatsky <daniel.bershatsky@gmail.com>
2022-01-01 12:53:19 +0300

    Implement linear layer with gaussian random projections

--- fewbit/modules/linear.py
+++ fewbit/modules/linear.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 
 from typing import Optional
 
-__all__ = ('LinearCRS', )
+__all__ = ('LinearCRS', 'LinearGRP')
 
 
 class LinearCRSFunc(T.autograd.Function):
@@ -49,9 +49,6 @@ class LinearCRSFunc(T.autograd.Function):
         return (input_grad, weight_grad, bias_grad, None)
 
 
-linear_crs = LinearCRSFunc.apply
-
-
 class LinearCRS(T.nn.Linear):
     """Class LinearCRS defines PyTorch layer based torch.nn.Linear module. It
     approximate gradients (of weights) with column-row sampling (CRS) and
@@ -73,3 +70,61 @@ class LinearCRS(T.nn.Linear):
 
     def extra_repr(self) -> str:
         return f'{super().extra_repr()} + nopairs={self.nopairs}'
+
+
+class LinearGRPFunc(T.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, input: T.Tensor, weight: T.Tensor,
+                bias: Optional[T.Tensor], proj_features: int) -> T.Tensor:
+        in_features = weight.shape[1]
+        proj = T.randn((proj_features, in_features))
+        input_proj = input @ proj.T
+        ctx.save_for_backward(input_proj, weight, bias, proj)
+        return F.linear(input, weight, bias)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input_proj, weight, bias, proj = ctx.saved_tensors
+
+        input_grad = None
+        if ctx.needs_input_grad[0]:
+            input_grad = grad_output @ weight
+
+        weight_grad = None
+        if ctx.needs_input_grad[1]:
+            weight_grad = np.einsum('...i,...j->ij', input_proj, grad_output)
+            weight_grad = proj.T @ weight_grad
+
+        bias_grad = None
+        if ctx.needs_input_grad[2]:
+            bias_grad = T.einsum('...i->i', grad_output)
+
+        return (input_grad, weight_grad, bias_grad, None)
+
+
+class LinearGRP(T.nn.Linear):
+    """Class LinearGRP defines PyTorch layer based torch.nn.Linear module. It
+    approximate gradients (of weights) with Gaussian random projections (GRP)
+    and stored subset of input columns for backward phase.
+    """
+
+    def __init__(self,
+                 in_features: int,
+                 out_features: int,
+                 bias: bool = True,
+                 device=None,
+                 dtype=None,
+                 proj_features: Optional[int] = None) -> None:
+        super().__init__(in_features, out_features, bias, device, dtype)
+        self.proj_features: int = proj_features or out_features // 2
+
+    def forward(self, input: T.Tensor) -> T.Tensor:
+        return linear_crs(input, self.weight, self.bias, self.proj_features)
+
+    def extra_repr(self) -> str:
+        return f'{super().extra_repr()} + proj_features={self.nopairs}'
+
+
+linear_crs = LinearCRSFunc.apply
+linear_grp = LinearGRPFunc.apply
--- fewbit/modules/linear_test.py
+++ fewbit/modules/linear_test.py
@@ -5,42 +5,48 @@ from unittest import TestCase
 
 import torch as T
 
-from fewbit.modules.linear import LinearCRS
+from fewbit.modules.linear import LinearCRS, LinearGRP
 
 
-class TestLinearCRS(TestCase):
+def clone_module(module: T.nn.Module) -> T.nn.Module:
+    cloned = T.nn.Linear(module.in_features, module.out_features,
+                         module.bias is not None)
+    for name, param in cloned.named_parameters():
+        param.data = getattr(module, name).clone()
+    return cloned
 
-    @staticmethod
-    def clone_module(module: T.nn.Module) -> T.nn.Module:
-        cloned = T.nn.Linear(module.in_features, module.out_features,
-                             module.bias is not None)
-        for name, param in cloned.named_parameters():
-            param.data = getattr(module, name).clone()
-        return cloned
 
-    @staticmethod
-    def estimate_grad(module: T.nn.Module, xs: T.Tensor, repeat: int = 1):
-        weight_grad = T.zeros_like(module.weight)
-        if (with_bias := module.bias is not None):
-            bias_grad = T.zeros_like(module.bias)
+def estimate_grad(module: T.nn.Module, xs: T.Tensor, repeat: int = 1):
+    weight_grad = T.zeros_like(module.weight)
+    if (with_bias := module.bias is not None):
+        bias_grad = T.zeros_like(module.bias)
 
-        for _ in range(repeat):
-            module.zero_grad()
-            xs.grad = None
-            ys = module(xs)
-            ys.backward(T.ones_like(ys))
-            weight_grad += module.weight.grad
-            if with_bias:
-                bias_grad += module.bias.grad
-
-        weight_grad /= repeat
+    for _ in range(repeat):
+        module.zero_grad()
+        xs.grad = None
+        ys = module(xs)
+        ys.backward(T.ones_like(ys))
+        weight_grad += module.weight.grad
         if with_bias:
-            bias_grad /= repeat
+            bias_grad += module.bias.grad
 
-        if with_bias:
-            return xs.grad, weight_grad, bias_grad
-        else:
-            return xs.grad, weight_grad, None
+    weight_grad /= repeat
+    if with_bias:
+        bias_grad /= repeat
+
+    if with_bias:
+        return xs.grad, weight_grad, bias_grad
+    else:
+        return xs.grad, weight_grad, None
+
+
+class LinearTestCaseMixin:
+
+    def __init_subclass__(cls) -> None:
+        cls.layer_name = cls.__name__.removeprefix('Test')
+        cls.layer_ctor = globals().get(cls.layer_name, None)
+        if cls.layer_ctor is None:
+            raise RuntimeError(f'Layer {cls.layer_name} is not imported.')
 
     def setUp(self):
         self.rng = T.random.manual_seed(42)
@@ -48,8 +54,8 @@ class TestLinearCRS(TestCase):
     def test_forward(self):
         for bias in (False, True):
             with self.subTest(bias=bias):
-                module = LinearCRS(8, 4, bias)
-                module_cloned = self.clone_module(module)
+                module = self.layer_ctor(8, 4, bias)
+                module_cloned = clone_module(module)
 
                 with T.no_grad():
                     xs = T.randn((128, 8))
@@ -62,12 +68,12 @@ class TestLinearCRS(TestCase):
     def test_backward(self):
         for bias in (False, True):
             with self.subTest(bias=bias):
-                module = LinearCRS(8, 4, bias)
-                module_cloned = self.clone_module(module)
+                module = self.layer_ctor(8, 4, bias)
+                module_cloned = clone_module(module)
 
                 xs = T.randn((128, 8), requires_grad=True)
-                ys, ps, ss = self.estimate_grad(module, xs, 4096)
-                zs, qs, ts = self.estimate_grad(module_cloned, xs)
+                ys, ps, ss = estimate_grad(module, xs, 4096)
+                zs, qs, ts = estimate_grad(module_cloned, xs)
 
                 err_input = T.linalg.norm(ys - zs) / T.linalg.norm(zs)
                 err_weight = T.linalg.norm(ps - qs) / T.linalg.norm(qs)
@@ -77,3 +83,11 @@ class TestLinearCRS(TestCase):
                 if bias:
                     err_bias = T.linalg.norm(ss - ts) / T.linalg.norm(ts)
                     self.assertAlmostEqual(0, err_bias.item(), delta=1e-6)
+
+
+class TestLinearCRS(TestCase, LinearTestCaseMixin):
+    pass
+
+
+class TestLinearGRP(TestCase, LinearTestCaseMixin):
+    pass
